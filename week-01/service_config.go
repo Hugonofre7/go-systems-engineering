45a608f8d4a6b2f7867463290de8b9bad6ab27bf
Hugonofre <hugoant7@gmail.com>
2026-08-07 23:24:28 -0600

    add combinable service flags

--- week-01/service_config.go
+++ week-01/service_config.go
@@ -13,10 +13,24 @@ const (
 
 var currentLevel LogLevel = Info
 
+type ServiceFlag int
+
+const (
+	FlagTLS ServiceFlag = 1 << iota
+	FlagAuth
+	FlagRateLimited
+)
+
+var activeFlags ServiceFlag = FlagTLS | FlagAuth
+
 func main() {
 	if currentLevel >= Warn {
 		fmt.Println("High severity log level")
 	} else {
 		fmt.Println("Normal log level")
+	}
+
+	if activeFlags&FlagAuth != 0 {
+		fmt.Println("Authentication is enabled")
 	}
 }
