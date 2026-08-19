da404e0ed49d768e4ac42e19cebc4d40e8cf6ffa
Hugonofre <hugoant7@gmail.com>
2026-08-19 00:44:29 -0600

    complete cluster node methods and embedding

--- week-03/06-go-fundamentals/cluster_node.go
+++ week-03/06-go-fundamentals/cluster_node.go
@@ -2,7 +2,25 @@ package main
 
 import "fmt"
 
+type Logger struct {
+	Prefix string
+}
+
+func (l *Logger) Log(msg string) {
+	fmt.Printf("[%s] %s\n", l.Prefix, msg)
+}
+
+type Metrics struct {
+	RequestCount int
+}
+
+func (m *Metrics) Log(msg string) {
+	fmt.Printf("[metrics] %s (requests: %d)\n", msg, m.RequestCount)
+}
+
 type Node struct {
+	Logger
+	Metrics
 	ID      string
 	Healthy bool
 	Uptime  int
@@ -12,7 +30,7 @@ func (n *Node) SetHealthy(h bool) {
 	n.Healthy = h
 }
 
-func (n Node) Status() string {
+func (n *Node) Status() string {
 	return fmt.Sprintf(
 		"Node %s | Healthy: %t | Uptime: %ds",
 		n.ID,
@@ -21,22 +39,34 @@ func (n Node) Status() string {
 	)
 }
 
-func (n Node) BrokenSetHealthy(h bool) {
-	n.Healthy = h
+func (n *Node) IncrementUptime(seconds int) {
+	n.Uptime += seconds
 }
+
+//func (n Node) BrokenSetHealthy(h bool) {
+//	n.Healthy = h
+//}
 
 func main() {
 	node := Node{
+		Logger: Logger{
+			Prefix: "node-1",
+		},
+		Metrics: Metrics{
+			RequestCount: 42,
+		},
 		ID:      "node-1",
 		Healthy: false,
 		Uptime:  120,
 	}
 
-	node.BrokenSetHealthy(true)
-
-	fmt.Println("After BrokenSetHealthy:", node.Status())
+	//node.Log("test")
+	node.Logger.Log("test")
+	node.Metrics.Log("test")
 
 	node.SetHealthy(true)
+	node.IncrementUptime(30)
+	node.IncrementUptime(45)
 
-	fmt.Println("After SetHealthy:", node.Status())
+	fmt.Println(node.Status())
 }
