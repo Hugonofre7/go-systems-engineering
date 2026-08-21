ebe66bc52a167e8cb7c403825348ab1254ca3bb8
Hugonofre <hugoant7@gmail.com>
2026-08-21 00:01:17 -0600

    add concurrent health check worker

---
+++ week-04/cluster_dispatcher.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func healthCheckWorker(id int, jobs <-chan string, results chan<- string) {
+	for nodeName := range jobs {
+		result := fmt.Sprintf("worker %d: %s está OK", id, nodeName)
+		results <- result
+	}
+}
+
+func main() {
+	jobs := make(chan string)
+	results := make(chan string)
+
+	go healthCheckWorker(1, jobs, results)
+
+	go func() {
+		jobs <- "node-1"
+		jobs <- "node-2"
+		jobs <- "node-3"
+		close(jobs)
+	}()
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(<-results)
+	}
+}
