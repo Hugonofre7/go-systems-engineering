d6369ed297e352581e85b431137439f0931e2461
Hugonofre <hugoant7@gmail.com>
2026-08-22 00:41:44 -0600

    migrate workers to context cancellation

--- week-04/08-go-fundamentals/orchestrated_task_manager.go
+++ week-04/08-go-fundamentals/orchestrated_task_manager.go
@@ -1,31 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func longRunningTask(id int, jobID string, done <-chan struct{}, result chan<- string) {
+// func longRunningTask(id int, jobID string, done <-chan struct{}, result chan<- string) {
+func longRunningTask(id int, jobID string, ctx context.Context, result chan<- string) {
 	select {
 	case <-time.After(time.Duration(id) * time.Second):
 		result <- fmt.Sprintf("worker %d: job %s completado", id, jobID)
-	case <-done:
-		result <- fmt.Sprintf("worker %d: job %s cancelado", id, jobID)
+	//case <-done:
+	//	result <- fmt.Sprintf("worker %d: job %s cancelado", id, jobID)
+	case <-ctx.Done():
+		result <- fmt.Sprintf(
+			"worker %d: job %s cancelado: %v",
+			id,
+			jobID,
+			ctx.Err(),
+		)
 	}
 }
 
 func main() {
-	done := make(chan struct{})
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		1500*time.Millisecond,
+	)
+	defer cancel()
 	result := make(chan string)
 
 	for i := 1; i <= 3; i++ {
-		go longRunningTask(i, fmt.Sprintf("job_%d", i), done, result)
+		go longRunningTask(i, fmt.Sprintf("job-%d", i), ctx, result)
 	}
-
-	go func() {
-		time.Sleep(1500 * time.Millisecond)
-		close(done)
-	}()
 
 	for i := 0; i < 3; i++ {
 		fmt.Println(<-result)
